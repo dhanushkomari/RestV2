e5f9edc9fddd1c45e9851bb1bccac3c96f8e9b60
dhanushkomari <dhanushkomari@gmail.com>
2022-02-10 23:29:06 +0530

    push category api is added

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views as rest_views
 app_name = 'api'
 
 urlpatterns = [
-    path('push-oi/', rest_views.PushOIView, name = 'push-oi')
+    path('push-oi/', rest_views.PushOIView, name = 'push-oi'),
+    path('push-cat/', rest_views.PushCategory, name = 'push-cat'),
 ]
