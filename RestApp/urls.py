95bfa58b46846c0d286b91031838d25f78b7bd5e
dhanushkomari <dhanushkomari@gmail.com>
2022-02-11 22:04:41 +0530

    admin functionalties added

--- RestApp/urls.py
+++ RestApp/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
 
     path('select-chef/<int:id>', views.SelectChef, name = 'select-chef'),
     path('pending-alloc-status/<str:id>/<str:chef_id>',  views.PendingAllocStatus, name = 'pending-alloc-status'),
-    path('complete-alloc-status/<str:id>/<str:chef_id>',  views.CompleteAllocStatus, name = 'complete-alloc-status')
+    path('complete-alloc-status/<str:id>/<str:chef_id>',  views.CompleteAllocStatus, name = 'complete-alloc-status'),
+
+    path('admin-dashboard', views.AdminDashboardView, name = 'admin-dashboard'),
+    path('chef-reports/<int:id>', views.ChefReportsView, name = 'chef-reports'),
 
 ]
